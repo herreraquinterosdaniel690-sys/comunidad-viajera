8af22f74c0b71ac446a86c00db005ff206aa3546
Keidyn <herreraquinterosdaniel690@gmail.com>
2026-08-11 15:11:19 -0600

    mensaje para guardar el commit

--- middlewares/post-validator.js
+++ middlewares/post-validator.js
@@ -7,10 +7,31 @@ import { existePost } from '../helpers/db-validators.js'
 export const createPostValidator = [
     validateJWT,
     authtenticatedLimiter,
+    (req, res, next) => {
+        if (req.is('multipart/form-data')) {
+            if (!req.body.title) {
+                return res.status(400).json({ message: 'El titulo es obligatorio' });
+            }
+            if (req.body.title.length > 100) {
+                return res.status(400).json({ message: 'El titulo no debe de exceder los 100 caracteres' });
+            }
+            if (!req.body.content) {
+                return res.status(400).json({ message: 'El contenido es obligatorio' });
+            }
+            return next();
+        } else {
+            next();
+        }
+    },
     check('title', 'El titulo es obligatorio') .not ().isEmpty(),
     check('title', 'El titulo no debe de exceder los 100 caracteres').isLength({max: 100}),
     check('content', 'El contenido es obligatorio').not().isEmpty(),
-    validarCampos
+    (req, res, next) => {
+        if (req.is('multipart/form-data')) {
+            return next();
+        }
+        return validarCampos(req, res, next);
+    }
 ];
 
 export const getPostValidator = [
